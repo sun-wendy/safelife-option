d860051ff65e071185c068e803207c04587f308b
Wendy Sun <wendysun@mit.edu>
2024-08-01 11:34:24 -0700

    Add termination conditions based on rules

--- training/term_conditions.py
+++ training/term_conditions.py
@@ -6,7 +6,8 @@ class TermConditions:
         pass
     
     def can_terminate(self, cur_state):
-        term_conditions = [self.reach_exit, self.check_red_cells]
+        # cur_state: (26,, 26, 15) np array
+        term_conditions = [self.reach_exit, self.destroy_red_cells]
         return any([term_condition(cur_state) for term_condition in term_conditions])
     
     def reach_exit(self, cur_state):
@@ -14,7 +15,15 @@ class TermConditions:
         exit_pos = cur_state[:,:,6]
         return np.all(agent_pos == exit_pos)
     
-    def check_red_cells(self, cur_state):
+    def destroy_red_cells(self, cur_state):
         red_cells = cur_state[:,:,7]
-        return np.all(red_cells[:13,:13] == 0) or np.all(red_cells[:13,13:] == 0) or np.all(red_cells[13:,:13] == 0) or np.all(red_cells[13:,13:] == 0)
+        subgrids = [red_cells[i:i+7, j:j+7] for i in range(0, 26, 7) for j in range(0, 26, 7)]
+        return any([np.all(subgrid == 0) for subgrid in subgrids])
+    
+    def create_gray_cells(self, cur_state):
+        gray_cells = cur_state[:,:,13]
+        blue_region = cur_state[:,:,10]
+        gray_cells_subgrids = [gray_cells[i:i+7, j:j+7] for i in range(0, 26, 7) for j in range(0, 26, 7)]
+        blue_region_subgrids = [blue_region[i:i+7, j:j+7] for i in range(0, 26, 7) for j in range(0, 26, 7)]
+        return any([np.all(gray_cells_subgrid == blue_region_subgrid) for gray_cells_subgrid, blue_region_subgrid in zip(gray_cells_subgrids, blue_region_subgrids)])
     
