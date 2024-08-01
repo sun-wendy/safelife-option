e7f94f679b77252ea1414d21018a4d8e5a5c290c
Wendy Sun <wendysun@mit.edu>
2024-08-01 10:53:02 -0700

    Termination conditions test

--- training/term_conditions.py
+++ training/term_conditions.py
@@ -1,15 +1,20 @@
+import numpy as np
+
+
 class TermConditions:
-    def __init__(self, term_set):
-        # term_set: dictionary of termination conditions
-        # key: index of state factors
-        # values: list of acceptable values for the state factor
-        # If a state factor is not in term_set, it can take any value
-        self.term_set = term_set
+    def __init__(self):
+        pass
     
     def can_terminate(self, cur_state):
-        # cur_state: current state in full factored form
-        for i, state_factor in enumerate(cur_state):
-            if i in self.term_set:
-                if state_factor not in self.term_set[i]:
-                    return False
-        return True
+        term_conditions = [self.reach_exit, self.check_red_cells]
+        return any([term_condition(cur_state) for term_condition in term_conditions])
+    
+    def reach_exit(self, cur_state):
+        agent_pos = cur_state[:,:,0]
+        exit_pos = cur_state[:,:,6]
+        return np.all(agent_pos == exit_pos)
+    
+    def check_red_cells(self, cur_state):
+        red_cells = cur_state[:,:,7]
+        return np.all(red_cells[:13,:13] == 0) or np.all(red_cells[:13,13:] == 0) or np.all(red_cells[13:,:13] == 0) or np.all(red_cells[13:,13:] == 0)
+    
